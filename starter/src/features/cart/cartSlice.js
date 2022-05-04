1c8c466cb25f4698b615c91933fc13c10c6ee22d
Patrick Reid <patrickjreid@gmail.com>
2022-05-04 07:35:40 -0600

    added increase and decrease functionality with action payload

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -10,9 +10,27 @@ const initialState = {
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState
-})
+  initialState,
+  reducers:{
+    clearCart:(state) => {
+      state.cartItems = [];
+    }, 
+    removeItem: (state, action) => {
+      const itemId = action.payload
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id );
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id );
+      cartItem.amount = cartItem.amount - 1;
+    },
+  },
+});
 
 // console.log(cartSlice)
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
